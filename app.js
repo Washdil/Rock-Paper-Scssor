d39559e91c4dbde7483a16f2bdf7853a1b0cf0f8
Washdil Baloch <washdildm484@gmail.com>
2023-08-29 02:52:55 -0700

    made the ui

--- app.js
+++ app.js
@@ -1,33 +1,80 @@
-function getComputerChoice() {
-     let computerPick = Math.round(Math.random() * 10);
+const choices = document.querySelectorAll('.choice');
+const result = document.querySelector('.result');
+const playerScoreSpan = document.getElementById('playerScore');
+const computerScoreSpan = document.getElementById('computerScore')
+
+let playerScore = 0;
+let computerScore = 0;
+let gamesPlayed = 0;
+
+choices.forEach(choice => {
+     choice.addEventListener('click', startGame)
+});
+
+function startGame(e) {
+     const playerChoice = e.target.dataset.choice;
+     const computerPick = getcomputerPick();
+     const gameResult = getWinner(playerChoice, computerPick);
+     showResult(playerChoice, computerPick, gameResult)
      
-     if(computerPick <= 3 ) {
-          return 'Rock';
-     } else if(computerPick > 3 && computerPick <= 6) {
-          return 'Paper';
-     } else if(computerPick > 6 && computerPick <= 9) {
-          return 'Scissor';
-     } 
-}
- 
+    
+     gamesPlayed++;
+     
+     
 
-function playRound(playerMove , ComputerMove) {
-     if(playerMove === 'Rock' && ComputerMove === 'Scissor') {
-          return `You win ${playerMove} beats ${ComputerMove}`;
-     }
-     else if (playerMove === 'Paper' && ComputerMove === 'Rock') {
-          return `You win ${playerMove} beats ${ComputerMove} `;
-     }
 
-     else if (playerMove === 'Scissor' && ComputerMove === 'Paper') {
-          return `You win ${playerMove} beats ${ComputerMove}`;
-     }
-      else {
-          return ` You lose ${playerMove} loses to ${ComputerMove}`;
+     if(gamesPlayed >= 5) {
+      if(playerScore > computerScore) {
+        result.textContent = 'You won the match';
+      } else if(computerScore > playerScore) {
+        result.textContent= "Computer Won the match";
       }
- 
+      else {
+        result.textContent = "It's a tie";
+      }
+
+      
+      updateGame();
+      reset();
+     }
 }
 
-let playerMove = prompt('what is your move ');
-let ComputerMove = getComputerChoice();
-console.log(playRound(playerMove , ComputerMove));
+function reset() {
+  playerScore,computerScore,gamesPlayed = 0;
+}
+
+function updateGame() {
+  playerScoreSpan.innerHTML = `${playerScore++}`;
+  computerScoreSpan.innerHTML = `${computerScore++}`
+}
+
+function getcomputerPick() {
+     const choices = ['rock' , 'paper', 'scissor'];
+     const randomIndex = Math.floor(Math.random() * choices.length);
+     return choices[randomIndex];
+}
+function getWinner(playerChoice, computerPick) {
+     if (playerChoice === computerPick) {
+       return "It's a tie";
+     } else if (
+       (playerChoice === "rock" && computerPick === "scissor") ||
+       (playerChoice === "paper" && computerPick === "rock") ||
+       (playerChoice === "scissor" && computerPick === "paper")
+     ) {
+       return "You Win";
+     } else {
+       return "Computer wins";
+     }
+   }
+   
+   function showResult(playerChoice, computerPick, gameResult) {
+     result.innerHTML = `
+       You chose: ${playerChoice} <br>
+       Computer chose:${computerPick} <br>
+       Result:${gameResult};
+     `;
+   }
+
+
+
+
